338a787ab11b2a9bd95ddb23f2e64c0f3988cb6f
miftahuljannahhh <mj5416386@gmail.com>
2025-06-09 15:34:11 +0700

    ioFilewithCustomName

---
+++ ioFilewithCustomName.cpp
@@ -0,0 +1,56 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+    string namefile;
+
+    cout << "Masukkan Name File : ";
+    cin >> namefile;
+    cin.ignore();
+
+    // membuat objek output file stream.
+    ofstream outfile;
+    //membuka file untuk ditulisi.
+    outfile.open(namefile + ".txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis 
+    while (true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dala file 
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //Membuat objek input file stream
+    ifstream infile;
+    //Membuka file yang dituliskan
+    infile.open(namefile + ".txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    // Jika file ada maka
+    if (infile.is_open())
+    {
+        // Melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan disini
+            cout << baris << '\n';
+        }
+        // Tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // Jika tidak ditemukan file maka akan akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}
+    
