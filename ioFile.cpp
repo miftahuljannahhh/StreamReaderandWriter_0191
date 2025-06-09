46eb12a20ff6a079a7424daafc52f4cb72cf727d
miftahuljannahhh <mj5416386@gmail.com>
2025-06-09 14:39:12 +0700

    deklarasi variabel didalam main function

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 
 int main() {
-  
+    string baris;
+
+    // membuat objek output file stream.
+    ofstream outfile;
+    //membuka file untuk ditulisi.
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+   
 }
     
